7438ba227c2846f401e173d286ce04ffca5c860c
Fredmoz <fredtembemoz@gmail.com>
2022-07-04 16:48:49 +0200

    adicionar a rota api/test v4

--- pages/api/test.js
+++ pages/api/test.js
@@ -2,6 +2,7 @@ function test(request, response){
 const dynamicDate = new Date();
 
 response.json({
+    Message: "Hi mr James este eh uma api teste que retorna a a data e hora atual, brevemente o teras no link polegar group.com",
     Date: dynamicDate.toGMTString()
 })
 }
